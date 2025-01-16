36ca1305448cddce14323e44e8c3da367aab6988
Luke Nicklin <u88xtjl9@students.codeinstitute.net>
2025-01-16 09:46:32 +0000

    Update images

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Game choices
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+// Game logic
+
+
+// Score variables
+let  wins = 0;
+let losses = 0;
+let ties = 0;
+
+
+// Function to play the game
+function playGame()
+
+
+// Function to generate random choice for computer
+function computerMove()
+
+// Function to update scoreboard
+function updateScoreboard()
