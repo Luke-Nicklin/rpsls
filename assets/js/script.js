c01d52ae13fcd8dce9a536a47a8925874a81ea1f
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-01-20 16:29:37 +0000

    Add buttons and event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,31 @@
-const computerMove = document.getElementById ('computerrMove');
-const userMove = document.getElementById ('userMove');
-const resultDisplay = document.getElementById ('result');
+const rockButton = document.getElementById ('rock');
+const paperButton = document.getElementById ('paper');
+const scissorsButton = document.getElementById ('scissors');
+const lizardButton = document.getElementById ('lizard');
+const spockButton = document.getElementById ('spock');
 
+// Add event listeners to each button
+rockButton.addEventListener('click', () => {
+  // Rock button click
+  playGame('rock');
+});
 
+paperButton.addEventListener('click', () => {
+  // Paper button click
+  playGame('paper');
+});
 
+scissorsButton.addEventListener('click', () => {
+  // Scissors button click
+  playGame('scissors');
+});
 
+lizardButton.addEventListener('click', () => {
+  // Lizard button click
+  playGame('lizard');
+});
 
-function playGame(userMove) {
-  // Get computer's choice randomly
-  const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-  const computerMove = choices[Math.floor(Math.random() * choices.length)];
-
-  // Determine winner
-  let result;
-  if (userMove === computerMove) {
-    result = "tie";
-  } 
-
-  // Update scoreboard
-  
-
-  // Display images
-
-  
-  // Display result message
-}
+spockButton.addEventListener('click', () => {
+  // Spock button click
+  playGame('spock');
+});
