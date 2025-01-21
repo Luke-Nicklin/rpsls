233ee53f4e6cec3fd419455148df0162ede683dd
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-01-21 17:01:37 +0000

    Start JavaScript code again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,18 @@
-const rockButton = document.getElementById('rock');
-const paperButton = document.getElementById('paper');
-const scissorsButton = document.getElementById('scissors');
-const lizardButton = document.getElementById('lizard');
-const spockButton = document.getElementById('spock');
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const lizardButton = document.getElementById("lizard");
+const spockButton = document.getElementById("spock");
 
 function playGame(userMove) {
   // Get computer's move randomly
   const moves = ["rock", "paper", "scissors", "lizard", "spock"];
   const computerMove = moves[Math.floor(Math.random() * moves.length)];
 
-  // Determine winner
-  let result;
-  if (userMove === computerMove) {
-    result = "tie";
-  } else if (
-      (userMove === "rock" && 
-        (computerMove === "scissors" || computerMove === "lizard")) ||
-      (userMove === "paper" &&
-        (computerMove === "rock" || computerMove === "spock")) ||
-      (userMove === "scissors" &&
-        (computerMove === "paper" || computerMove === "lizard")) ||
-      (userMove === "lizard" &&
-        (computerMove === "paper" || computerMove === "spock")) ||
-      (userMove === "spock" &&
-        (computerMove ==="rock" || computerMove === "scissors"))
-    ) {
-      result = "win";
-    } else {
-      result = "loss";
-    }
+document.getElementById("rock").addEventListener("click", playGame);
+document.getElementById("paper").addEventListener("click", playGame);
+document.getElementById("scissors").addEventListener("click", playGame);
+document.getElementById("lizard").addEventListener("click", playGame);
+document.getElementById("spock").addEventListener("click", playGame);
 
-console.log(computerMove, result);
-  
-  // Update scoreboard
- let wins = parseInt(document.getElementById("wins").textContent);
- let losses = parseInt(document.getElementById("losses").textContent);
- let ties = parseInt(document.getElementById("ties").textContent);
-
- // Update result
- let resultDisplay = parseInt(document.getElementById("resultDisplay").textContent);
-
- // Add scores to scoreboard
- if (result === "win") {
-  wins++
- } else if (result === "loss") {
-  losses++
- } else {
-  ties++
- }
 }
