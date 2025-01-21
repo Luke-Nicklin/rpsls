7d4c90347d9f6dc4bf81e02e4f4a6cafe72a121c
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-01-21 11:14:37 +0000

    Add else if statement to determine winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,31 @@ const scissorsButton = document.getElementById('scissors');
 const lizardButton = document.getElementById('lizard');
 const spockButton = document.getElementById('spock');
 
-console.log(rockButton);
-
 function playGame(userMove) {
   // Get computer's choice randomly
-  const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-  const computerMove = choices[Math.floor(Math.random() * choices.length)];
+  const moves = ["rock", "paper", "scissors", "lizard", "spock"];
+  const computerMove = moves[Math.floor(Math.random() * moves.length)];
 
   // Determine winner
-}
+  let result;
+  if (userMove === computerMove) {
+    result = "tie";
+  } else if (
+      (userMove === "rock" && 
+        (computerMove === "scissors" || computerMove === "lizard")) ||
+      (userMove === "paper" &&
+        (computerMove === "rock" || computerMove === "spock")) ||
+      (userMove === "scissors" &&
+        (computerMove === "paper" || computerMove === "lizard")) ||
+      (userMove === "lizard" &&
+        (computerMove === "paper" || computerMove === "spock")) ||
+      (userMove === "spock" &&
+        (computerMove ==="rock" || computerMove === "scissors"))
+    ) {
+      result = "win";
+    } else {
+      result = "loss";
+    }
+
+    console.log(computerMove, result);
+  }
