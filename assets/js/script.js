37f18d3b624c1a15a35bfb6b84e5b706da388d55
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-01-22 09:46:06 +0000

    Add button elements, array of possible moves and function to play game

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,19 @@ const scissorsButton = document.getElementById("scissors");
 const lizardButton = document.getElementById("lizard");
 const spockButton = document.getElementById("spock");
 
+// Array  possible moves
+const moves = ["rock", "paper", "scissors", "lizard", "spock"];
+
+// Function to play game
 function playGame(userMove) {
-  // Get computer's move randomly
-  const moves = ["rock", "paper", "scissors", "lizard", "spock"];
-  const computerMove = moves[Math.floor(Math.random() * moves.length)];
+  const computerMove = moves[Math.floor(Math.random() * moves.length)]
 
-document.getElementById("rock").addEventListener("click", playGame);
-document.getElementById("paper").addEventListener("click", playGame);
-document.getElementById("scissors").addEventListener("click", playGame);
-document.getElementById("lizard").addEventListener("click", playGame);
-document.getElementById("spock").addEventListener("click", playGame);
+// Event listeners for buttons
+rockButton.addEventListener("click", playGame);
+paperButton.addEventListener("click", playGame);
+scissorsButton.addEventListener("click", playGame);
+lizardButton.addEventListener("click", playGame);
+spockButton.addEventListener("click", playGame);
 
+console.log(playGame);
 }
