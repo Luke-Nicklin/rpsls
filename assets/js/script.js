efd887ef3a608fd29b6dc4b1f6870b58914303b1
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-01-22 09:58:50 +0000

    Add else if statement to determine winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,28 @@ const scissorsButton = document.getElementById("scissors");
 const lizardButton = document.getElementById("lizard");
 const spockButton = document.getElementById("spock");
 
-// Array  possible moves
+// Array of possible moves
 const moves = ["rock", "paper", "scissors", "lizard", "spock"];
 
 // Function to play game
 function playGame(userMove) {
   const computerMove = moves[Math.floor(Math.random() * moves.length)]
 
-// Event listeners for buttons
-rockButton.addEventListener("click", playGame);
-paperButton.addEventListener("click", playGame);
-scissorsButton.addEventListener("click", playGame);
-lizardButton.addEventListener("click", playGame);
-spockButton.addEventListener("click", playGame);
+// Else if statement to determine result
+let result;
+if (userMove === computerMove) {
+  result = "Tie";
+} else if (
+  (userMove === "rock" && (computerMove === "scissors" || computerMove === "lizard")) ||
+  (userMove === "paper" && (computerMove === "rock" || computerMove === "spock")) ||
+  (userMove === "scissors" && (computerMove === "paper" || computerMove === "lizard")) ||
+  (userMove === "lizard" && (computerMove === "paper" || computerMove === "spock")) ||
+  (userMove === "spock" && (computerMove === "rock" || computerMove === "scissors"))
+) {
+  result = "win";
+} else {
+  result = "lose";
+}
 
-console.log(playGame);
+console.log(computerMove, result);
 }
